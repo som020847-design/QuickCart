bc883c3eef4150e9c4ff3d909817eae7baeffeed
Nualwan <your-email@gmail.com>
2026-02-07 20:10:39 +0700

    Save

--- models/User.js
+++ models/User.js
@@ -5,8 +5,8 @@ const userSchema = new mongoose.Schema({
     _id:{ type : String, required:true},
     name: { type : String, required:true},
     email: { type : String, required:true, unique:true},
-    imageUrl :{ type : String, required:true},
-    cartItems :{ type : Object, default: {}}
+    imageUrl:{ type : String, required:true},
+    cartItems:{ type : Object, default: {}}
 
 },{ minimize: false})
 
