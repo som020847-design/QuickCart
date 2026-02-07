84c6bcb9fd2bbab6327a3036f828dc6d9901f6fd
Nualwan <your-email@gmail.com>
2026-02-07 21:23:35 +0700

    Finished Project

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -17,7 +17,7 @@ export async function POST(request) {
     // calculate amount using items
     const amount = await items.reduce(async (acc, item) => {
       const product = await Product.findById(item.product);
-      return acc + product.offerPrice *item.quantity;
+      return await acc + product.offerPrice *item.quantity;
     },0)
     await inngest.send({
         name: 'order/created',
